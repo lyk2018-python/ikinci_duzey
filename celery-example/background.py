b5406b13f779aa7d6b9b0c76f3bad91914a9acbb
Umut Karcı <cediddi@gmail.com>
2018-07-27 10:07:53 +0300

    topla ve çıkar

--- celery-example/background.py
+++ celery-example/background.py
@@ -2,8 +2,24 @@ from celery import Celery
 
 app = Celery(__name__, backend="amqp://localhost", broker="redis://localhost")
 
+
 @app.task
-def multiple(x,y):
-    z = x**y
+def multiple(x, y):
+    z = x ** y
     print(z)
     return z
+
+
+@app.task
+def topla(x, y):
+    return x + y
+
+
+@app.task
+def cikar(a, b):
+    return a - b
+
+
+"""
+chord([topla.s(1, 2), topla.s(3, 4)], cikar.s())
+"""
